434534198eff73e6022181f3c4e6850be5d13216
Adrian Matei <ama@codingpedia.org>
2017-06-16 07:25:20 +0200

    insert into mongodb successfully - looks right to me but not being right displayed

--- src/main/java/org/codingmarks/freeprogrammingbooksimport/ImportFreeProgrammingBooks.java
+++ src/main/java/org/codingmarks/freeprogrammingbooksimport/ImportFreeProgrammingBooks.java
@@ -7,13 +7,17 @@ import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 import sun.misc.Launcher;
 
+import javax.sound.midi.SysexMessage;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Properties;
+
+import static com.mongodb.client.model.Filters.eq;
 
 /**
  * Created by ama on 13.06.17.
@@ -30,13 +34,13 @@ public class ImportFreeProgrammingBooks {
         MongoClient mongoClient = new MongoClient(connectionString);
         MongoDatabase database = mongoClient.getDatabase("codingpedia-bookmarks");
 
-        MongoCollection<Document> collection = database.getCollection("bookmarks");
+        MongoCollection<Document> bookmarksCollection = database.getCollection("bookmarks");
 
-        System.out.println("number of bookmarks :" + collection.count());
+        System.out.println("number of bookmarks :" + bookmarksCollection.count());
 
 
         //read file line by line
-        InputStream freeProgrammingBooksStream = ImportFreeProgrammingBooks.class.getClassLoader().getResourceAsStream("free-programming-books.md");
+        InputStream freeProgrammingBooksStream = ImportFreeProgrammingBooks.class.getClassLoader().getResourceAsStream("free-programming-books-test.md");
         BufferedReader br = new BufferedReader(new InputStreamReader(freeProgrammingBooksStream, "UTF-8"));
 
         String line;
@@ -55,10 +59,17 @@ public class ImportFreeProgrammingBooks {
                     category = getCategory(line);
                     System.out.println("category : " + category);
                 } else if(line.contains("[")){
+
+                    Document document = new Document();
+
                     String title = line.substring(line.indexOf("[")+1,line.indexOf("]"));
                     System.out.println("title : " + title);
-                    String url = line.substring(line.indexOf("(")+1,line.indexOf(")"));
-                    System.out.println("url : " + url);
+                    document.append("name", title);
+
+                    String location = line.substring(line.indexOf("(")+1,line.indexOf(")"));
+                    System.out.println("url : " + location);
+                    document.append("location", location);
+
                     List<String> tags = new ArrayList<>();
                     tags.add("free-programming-books");//standard tag for all books
                     tags.add(category);//all links have at least one category (fall under an ### element)
@@ -66,6 +77,7 @@ public class ImportFreeProgrammingBooks {
                         tags.add(subCategory);
                     }
                     System.out.println("tags : " + tags);
+                    document.append("tags", tags);
 
                     //build description
                     String description = line.substring(line.indexOf(")") + 1, line.length());
@@ -75,6 +87,41 @@ public class ImportFreeProgrammingBooks {
                             description = description.substring(1, description.length()).trim();
                         }
                         System.out.println("description : " + description);
+                        document.append("description", description);
+
+                        String descriptionHTML = "<p>" + description + "</p>";
+                        System.out.println("descriptionHTML : " + descriptionHTML);
+                        document.append("descriptionHtml", descriptionHTML);
+                    }
+
+                    //add createdAt
+                    Date now = new Date();
+                    System.out.println("createdAt: " + now);
+                    document.append("createdAt", now);
+
+                    //shared
+                    System.out.println("shared:" + true);
+                    document.append("shared", true);
+
+                    //userId
+                    System.out.println("userId:" + configProperties.getProperty("userId"));
+                    document.append("userId", configProperties.getProperty("userId"));
+
+
+                    //Add the document to mongoDB
+                    Document doc = new Document("name", title)
+                            .append("location", location)
+                            .append("description", 1)
+                            .append("info", new Document("x", 203).append("y", 102));
+
+                    //verify if the document is present
+                    Document bookmark = bookmarksCollection.find(eq("location", location)).first();
+                    if(bookmark!=null){
+                        System.out.println("*********************** Bookmark already present *********************** ");
+                        System.out.println(bookmark.toJson());
+                    } else {
+                        bookmarksCollection.insertOne(document);
+                        System.out.println("------------------- Bookmark successfuly inserted ------------------- ");
                     }
 
                     System.out.println("\n ");
